2f6c6fa4359fc9ee82b7975ddec41fe7c7d6dd01
Aaron <alandrews3@alaska.edu>
2015-03-03 17:36:41 -0900

    Got the game player to compile and put in the GPL notice at the start of each file for the game player.

--- include/profile.h
+++ include/profile.h
@@ -1,3 +1,20 @@
+/*Copywrite 2014 Shaun Bond, Jeffery Waldon, Charlie Carlson, Simon Smith, Joseph DeWilde, Aaron Andrews*/
+
+/*This file is part of Dina'ki Adventures.
+
+Dina'ki Adventures is free software : you can redistribute it and / or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Dina'ki Adventures is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Dina'ki Adventures. If not, see <http://www.gnu.org/licenses/>.*/
+
 // Profile class for profile object
 // Created by Charlie Carlson
 // Scrum 1: Feb 18th 2014
--- src/profilecontroller.cpp
+++ src/profilecontroller.cpp
@@ -7,12 +7,32 @@
 #include "scenecontroller.h"
 #include "scene.h"
 #include "map.h"
+/*Copywrite 2014 Shaun Bond, Jeffery Waldon, Charlie Carlson, Simon Smith, Joseph DeWilde, Aaron Andrews*/
+
+/*This file is part of Dina'ki Adventures.
+
+Dina'ki Adventures is free software : you can redistribute it and / or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Dina'ki Adventures is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Dina'ki Adventures. If not, see <http://www.gnu.org/licenses/>.*/
+
 #include "mapcontroller.h"
 
 #include <wctype.h>
 #include <wchar.h>
 #include <vector>
 #include <iostream>
+
+#include <SFML\Graphics\Color.hpp>
+#include <SFML\Audio\SoundSource.hpp>
 
 ProfileController::ProfileController(std::shared_ptr<ProfileWrangler> wrangler, std::shared_ptr<Profile> profile,
                   std::shared_ptr<Interface> interface,
